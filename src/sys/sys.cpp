c2ae050d0abf2f59d2bb5f52c683990b3c424fd3
CharellkingQu <CharellkingQu@gmail.com>
2014-05-20 10:48:47 +0800

    sys,添加fork

---
+++ src/sys/sys.cpp
@@ -0,0 +1,24 @@
+/* COPYRIGHT:   Copyright 2014 CharellkingQu
+ * LICENSE:     GPL
+ * AUTHOR:      CharellkingQu
+ * DATE :       2014-05-20
+ * Description: about process、thread and so on
+ */
+
+#include "sys/sys.h"
+
+#include <errno.h>
+
+#include "global/error.h"
+
+pid_t TimePass::Sys::Fork() {
+    pid_t pid = fork();
+    if (pid < 0) {
+        SET_ERRNO(errno);
+    }
+    return pid;
+}
+
+
+
+
---
+++ src/sys/sys.h
@@ -0,0 +1,24 @@
+/* COPYRIGHT:   Copyright 2014 CharellkingQu
+ * LICENSE:     GPL
+ * AUTHOR:      CharellkingQu
+ * DATE :       2014-05-20
+ * Description: about process、thread and so on
+ */
+
+#ifndef _SYS_SYS_H_
+#define _SYS_SYS_H_
+
+#include <sys/types.h>
+#include <unistd.h>
+
+namespace TimePass {
+namespace Sys {
+pid_t Fork();
+
+}; /*namespace Sys*/
+}; /*namespace TimePass*/
+
+
+
+
+#endif /* _SYS_SYS_H_ */
