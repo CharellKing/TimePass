0819f9bc34e45f7570de0b1347a96733f9b174b1
CharellKing <CharellkingQu@gmail.com>
2014-05-20 19:21:21 +0800

    解决子进程僵死的问题

--- example/echosvr/main.cpp
+++ example/echosvr/main.cpp
@@ -26,11 +26,22 @@
 
 #define SA reinterpret_cast<sockaddr*>
 
+void SigChild(int signo) {
+    int pid = 0;
+    int stat = 0;
+    if (TimePass::Sys::Wait(&stat, &pid)) {
+        printf("child %d terminate\n", pid);
+    } else {
+        printf("%s\n", TimePass::Error::GetLastErrmsg().c_str());
+    }
+    return;
+}
+
 void EchoClient(int connfd) {
     size_t n = 0;
     char buff[MAX_LINE];
 
-    while (TimePass::SockBase::Read(connfd, buff, MAX_LINE, &n)) {
+    while (TimePass::SockBase::Read(connfd, buff, MAX_LINE, &n) && n > 0) {
         if (false == TimePass::SockBase::Write(connfd, buff, n, NULL)) {
             printf("%s\n", TimePass::Error::GetLastErrmsg().c_str());
         }
@@ -64,6 +75,8 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
+    TimePass::Sys::Signal(SIGCHLD, SigChild);
+
     while (true) {
         connfd = TimePass::SockBase::Accept(listenfd, SA(NULL), NULL);
         if (connfd < 0) {
@@ -73,7 +86,10 @@ int main(int argc, char **argv) {
             if (0 == childpid) {
                 TimePass::SockBase::Close(listenfd);
                 EchoClient(connfd);
+                printf("子进程退出\n");
+                exit(0);
             }
+            TimePass::SockBase::Close(connfd);
         }
     }
     return 0;
