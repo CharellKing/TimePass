f07a90b447808ddb7bcc309af7b043ff48dfe5fc
CharellkingQu <CharellkingQu@gmail.com>
2014-05-21 18:11:23 +0800

    错误码分类加前缀

--- src/global/errno.h
+++ src/global/errno.h
@@ -10,24 +10,24 @@
 
 namespace TimePass {
 namespace ErrorNo {
-/*errno*/
-const int INDEX_EXCEED = 19881001; /*the index exceed*/
-const int INDEX_NONEXIST = 19881002; /*the index not exist*/
-const int EXPAND_CAPACITY_TO_MAX = 19881003; /*the capacity reach to max*/
-const int EXPAND_CAPACITY_FAILED = 19881004; /*expand capacity failed*/
-const int KEY_EXIST = 19881004; /*the data with the key existed*/
-const int SPACE_SHORTAGE = 19881005; /*the space is shortage*/
 
-const int LOCK_STATUS = 19881006; /*the lock's status error*/
-const int LOCK_EMPTY  = 19881007; /*the lock's operator is not necessary*/
-const int SIZE_NONZERO = 19881008; /*the size must be non-zero*/
-const int EMPTY_PTR    = 19881009; /*empty pointer*/
-const int TIME_EXPIRE  = 19881010; /*time expire*/
-const int SIZE_NONFIT = 19881011; /*size not fit*/
-const int CONTAINER_ISEMPTY = 19881012; /*容器为空*/
+const int NORMAL_SIZE_NONZERO = 19881001;            /*size不能为0*/
+const int NORMAL_PTR_NULL     = 19881002;            /*指针为空*/
+const int NORMAL_SIZE_NONFIT  = 19881003;            /*size不合适*/
+
+const int CONTAINER_INDEX_EXCEED        = 19891001;  /*容器索引超过限制*/
+const int CONTAINER_INDEX_NONEXIST      = 19891002;  /*容器索引不存在*/
+const int CONTAINER_CAPACITY_EXCEED     = 19891003;  /*容器容量达到上限*/
+const int CONTAINER_KEY_EXIST           = 19891004;  /*容器KEY已经存在*/
+const int CONTAINER_CAPACITY_SHORTAGE   = 19891005;  /*容器空间不足*/
+const int CONTAINER_SIZE_ZERO           = 19891006;  /*容器size为0*/
+
+const int LOCK_STATUS_UNKNOWN = 19901001;            /*未知的锁状态*/
+
+const int TIMER_TIME_EXPIRE = 19911001;              /*超时*/
 
 
-};
-};
+}; /*namespace ErrorNo*/
+}; /*namespace TimePass*/
 
 #endif /* _GLOBAL_ERRNO_H_ */
