26e9e22c9b7c659069f8fcbb09d0f0501f93d995
CharellkingQu <CharellkingQu@gmail.com>
2014-05-22 13:15:01 +0800

    添加完美注释

--- src/sys/sys.cpp
+++ src/sys/sys.cpp
@@ -15,9 +15,10 @@
 
 #include "global/error.h"
 
-/*
+/**
  * 创建子进程
- * */
+ * @return： 返回0，表示创建成功；-1，表示失败
+ */
 pid_t TimePass::Sys::Fork() {
     pid_t pid = fork();
     if (pid < 0) {
@@ -26,17 +27,22 @@ pid_t TimePass::Sys::Fork() {
     return pid;
 }
 
-/*
+/**
  * 捕获系统异常信号
- * */
+ * @param signum 信号id
+ * @param handler 捕获到信号的处理函数
+ * @return 返回处理函数，为NULL则为空
+ */
 sighandler_t TimePass::Sys::Signal(int signum, sighandler_t handler) {
     return signal(signum, handler);
 }
 
-/*
- * 等待进程的结束
- * p_pid可以为NULL，表示不获取子进程的ID
- * */
+/**
+ * 等待子进程状态改变
+ * @param p_status 返回子进程的状态
+ * @param p_child_pid 不为NULL，返回子进程的进程id；否则不返回
+ * @return true - 成功，false失败，用Error获取错误信息
+ */
 bool TimePass::Sys::Wait(int* p_status, int* p_child_pid) {
     int child_pid = wait(p_status);
     if (child_pid < 0) {
@@ -50,8 +56,13 @@ bool TimePass::Sys::Wait(int* p_status, int* p_child_pid) {
     return true;
 }
 
-/*
- * 针对进程id，暂停某进程执行
+/**
+ * 针对某个子进程，来等待其状态的改变
+ * @param pid 子进程id
+ * @param option 参数选项，设置成WNOHANG，表示没有子进程退出就立即返回
+ * @param p_status 返回紫禁城的状态
+ * @param p_pid 不为NULL，返回子进程的进程id；否则不返回
+ * @return true为成功， false为失败，用Error获取错误信息
  */
 bool TimePass::Sys::Wait(int pid, int option, int* p_status, int* p_pid) {
     int ret = waitpid(pid, p_status, option);
@@ -65,8 +76,10 @@ bool TimePass::Sys::Wait(int pid, int option, int* p_status, int* p_pid) {
     return true;
 }
 
-/*
- * 睡眠以微妙为单位
+/**
+ * 以微妙来设置睡眠时间
+ * @param usec 睡眠时间
+ * @return true为成功，false为失败，用Error获取错误信息
  */
 bool TimePass::Sys::USleep(int32_t usec) {
     struct timeval tv = {0, usec};
--- src/sys/sys.h
+++ src/sys/sys.h
@@ -15,29 +15,43 @@
 
 namespace TimePass {
 namespace Sys {
-/*
+
+/**
  * 创建子进程
- * */
+ * @return： 返回0，表示创建成功；-1，表示失败
+ */
 pid_t Fork();
 
-/*
+/**
  * 捕获系统异常信号
- * */
+ * @param signum 信号id
+ * @param handler 捕获到信号的处理函数
+ * @return 返回处理函数，为NULL则为空
+ */
 sighandler_t Signal(int signum, sighandler_t handler);
 
-/*
- * 等待进程的结束
- * p_pid可以为NULL，表示不获取子进程的ID
- * */
+/**
+ * 等待子进程状态改变
+ * @param p_status 返回子进程的状态
+ * @param p_child_pid 不为NULL，返回子进程的进程id；否则不返回
+ * @return true - 成功，false失败，用Error获取错误信息
+ */
 bool Wait(int* p_status, int* p_child_pid);
 
-/*
- * 针对进程id，暂停某进程执行
+/**
+ * 针对某个子进程，来等待其状态的改变
+ * @param pid 子进程id
+ * @param option 参数选项，设置成WNOHANG，表示没有子进程退出就立即返回
+ * @param p_status 返回紫禁城的状态
+ * @param p_pid 不为NULL，返回子进程的进程id；否则不返回
+ * @return true为成功， false为失败，用Error获取错误信息
  */
 bool Wait(int pid, int option, int* p_status, int* p_pid);
 
-/*
- * 睡眠以微妙为单位
+/**
+ * 以微妙来设置睡眠时间
+ * @param usec 睡眠时间
+ * @return true为成功，false为失败，用Error获取错误信息
  */
 bool USleep(int32_t usec);
 
