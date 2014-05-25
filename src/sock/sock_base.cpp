a15c8db52308b785e1cf37fba7071ed4134bf49c
CharellKing <CharellkingQu@gmail.com>
2014-05-25 13:22:11 +0800

    chinese comment to english comment and add getsockopt function

--- src/sock/sock_base.cpp
+++ src/sock/sock_base.cpp
@@ -11,8 +11,13 @@
 
 #include "global/error.h"
 
-
-/*创建套接字*/
+/**
+ * create socket
+ * @param family: a domain of communication. such as AF_INET and so on
+ * @param type: a socket type. such as SOCK_STREAM、SOCK_DGRAM and so on
+ * @param protocol: a protocol of socket. gernerally specialized with zero.
+ * @return a socket descriptor
+ */
 int TimePass::SockBase::Socket(int family, int type, int protocol) {
     int sock = socket(family, type, protocol);
     if (sock < 0) {
@@ -22,7 +27,13 @@ int TimePass::SockBase::Socket(int family, int type, int protocol) {
     return socket(family, type, protocol);
 }
 
-/*连接*/
+/**
+ * connect to a remote host
+ * @param sockfd: a socket has been created
+ * @param p_addr: the remote host's address
+ * @param addrlen: the address's byte-size
+ * @return true for OK, false for failed and get error message by "Error"
+ */
 bool TimePass::SockBase::Connect(int sockfd,
                                  const struct sockaddr* p_addr,
                                  socklen_t addrlen) {
@@ -33,7 +44,13 @@ bool TimePass::SockBase::Connect(int sockfd,
     return true;
 }
 
-/*绑定*/
+/**
+ * bind a port or address
+ * @param sockfd: a socket has been created
+ * @param p_myaddr: the local host's address
+ * @param addrlen: the address's byte-size
+ * @return true for OK, false for failed and get error message by "Error"
+ */
 bool TimePass::SockBase::Bind(int sockfd,
                               struct sockaddr* p_myaddr,
                               socklen_t addrlen) {
@@ -44,7 +61,12 @@ bool TimePass::SockBase::Bind(int sockfd,
     return true;
 }
 
-/*监听*/
+/**
+ * listen the connection of a client
+ * @param sockfd: a binded socket
+ * @param backlog: the size of the max queue size for saving connections
+ * @return true for OK, false for failed and get error message by "Error"
+ */
 bool TimePass::SockBase::Listen(int sockfd, int backlog) {
     if (listen(sockfd, backlog) < 0) {
         SET_ERRNO(errno);
@@ -53,7 +75,13 @@ bool TimePass::SockBase::Listen(int sockfd, int backlog) {
     return true;
 }
 
-/*接收*/
+/**
+ * accept a connection from a client
+ * @param sockfd: a listend socket
+ * @param p_cliaddr: a known connection address
+ * @param p_addrlen: the address size
+ * @return a socket from connection
+ */
 int TimePass::SockBase::Accept(int sockfd,
                                struct sockaddr* p_cliaddr,
                                socklen_t* p_addrlen) {
@@ -65,7 +93,13 @@ int TimePass::SockBase::Accept(int sockfd,
     return acc_sock;
 }
 
-/*获取本地sock的协议地址*/
+/**
+ * get local socket address
+ * @param sockfd: the sockfd which has listend or accepted or connected
+ * @param p_localaddr: the local address
+ * @param p_addrlen: the address's byte-size
+ * @return true for OK, false for failed and get error message by "Error"
+ */
 bool TimePass::SockBase::GetSockName(int sockfd,
                                      struct sockaddr* p_localaddr,
                                      socklen_t* p_addrlen) {
@@ -76,7 +110,13 @@ bool TimePass::SockBase::GetSockName(int sockfd,
     return true;
 }
 
-/*获取远程sock的协议地址*/
+/**
+ * get remote socket address
+ * @param sockfd: the sockfd which has listend or accepted or connected
+ * @param p_peeraddr: the remote address
+ * @param p_addrlen: the address's byte-size
+ * @return true for OK, false for failed and get error message by "Error"
+ */
 bool TimePass::SockBase::GetPeerName(int sockfd,
                                      struct sockaddr* p_peeraddr,
                                      socklen_t* p_addrlen) {
@@ -87,7 +127,14 @@ bool TimePass::SockBase::GetPeerName(int sockfd,
     return true;
 }
 
-/*读取数据*/
+/**
+ * read data from socket cache
+ * @param sockfd: a socket which connection has established
+ * @param p_buff: a buffer to read
+ * @param max_size: the buffer's capacity
+ * @param p_len: the real reading data bytes, it can be set NULL.
+ * @return true for OK, false for failed and get error message by "Error"
+ */
 bool TimePass::SockBase::Read(int sockfd,
                               void* p_buff,
                               size_t max_size,
@@ -104,7 +151,14 @@ bool TimePass::SockBase::Read(int sockfd,
     return true;
 }
 
-/*写数据*/
+/**
+ * write data to socket cache
+ * @param sockfd: a socket which connection has established
+ * @param p_buff: a buff filled data to write
+ * @param max_len: the buffer capacity
+ * @param p_len: the real write data bytes
+ * @return true for OK, false for failed and get error message by "Error"
+ */
 bool TimePass::SockBase::Write(int sockfd,
                                const void* p_buff,
                                size_t max_size,
@@ -122,8 +176,14 @@ bool TimePass::SockBase::Write(int sockfd,
 }
 
 
-/*将主机地址转化为网络地址*/
-int TimePass::SockBase::Hton(int af, const char* p_host, void* p_dst) {
+/**
+ * convert host address to network address
+ * @param af: the family address
+ * @param p_host: host address
+ * @param p_dst: the network address for output
+ * @return true for OK, false for failed and get error message by "Error"
+ */
+bool TimePass::SockBase::Hton(int af, const char* p_host, void* p_dst) {
     if (inet_pton(af, p_host, p_dst) <= 0) {
         SET_ERRNO(errno);
         return false;
@@ -131,7 +191,11 @@ int TimePass::SockBase::Hton(int af, const char* p_host, void* p_dst) {
     return true;
 }
 
-/*关闭套接字*/
+/**
+ * close socket
+ * @param sockfd: the socket has been established
+ * @return true for OK, false for failed and get error message by "Error"
+ */
 bool TimePass::SockBase::Close(int sockfd) {
     if (close(sockfd) < 0) {
         SET_ERRNO(errno);
@@ -141,13 +205,31 @@ bool TimePass::SockBase::Close(int sockfd) {
 }
 
 /**
- * 关闭socket,与close不同的是，close必须引用为0才能发送FIN.而且它还可以采用半开关闭
- * @param sockfd 要关闭的套节字
- * @param howto  SHUT_RD，关闭读这一半；SHUT_WR，关闭写这一半；SHUT_RDWR，全部关闭
- * @return true - 成功；false - 失败；通过Error可以获取错误信息
+ * close socket; compared with "Close", close send FIN until socket reference to zero,
+ * but shutdown can close in half and have nothing with socket reference.
+ * @param sockfd the socket will shutdown
+ * @param howto  SHUT_RD，close read；SHUT_WR，close write；SHUT_RDWR, close read and write
+ * @return true for OK, false for failed and get error message by "Error"
  */
 bool TimePass::SockBase::Shutdown(int sockfd, int howto) {
     if (shutdown(sockfd, howto) < 0) {
+        SET_ERRNO(errno);
+        return false;
+    }
+    return true;
+}
+
+/**
+ * get socket option value
+ * @param sockfd: a socket has been created
+ * @param level: option level(SOL_SOCKET,IPPROTO_TCP and so on)
+ * @param opt: socket option
+ * @param optval: the value buffer
+ * @param p_vallen: buffer length
+ * @return
+ */
+bool TimePass::SockBase::GetSockOpt(int sockfd, int level, int opt, void* optval, socklen_t* p_vallen) {
+    if (getsockopt(sockfd, level, opt, optval, p_vallen) < 0) {
         SET_ERRNO(errno);
         return false;
     }
