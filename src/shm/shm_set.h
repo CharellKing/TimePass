b48f82b965213874aa35261b0853ef762719255d
CharellKing <CharellkingQu@gmail.com>
2014-07-30 23:06:26 +0800

    add shm_set

---
+++ src/shm/shm_set.h
@@ -0,0 +1,23 @@
+/*
+ * shm_set.h
+ *
+ *  Created on: Jul 30, 2014
+ *      Author: ck
+ */
+
+#ifndef _SHM_SHM_SET_H_
+#define _SHM_SHM_SET_H_
+
+#include "shm/shm_rbtree.h"
+
+namespace TimePass {
+template <typename T, typename EXTEND>
+class ShmSet {
+ public:
+
+ private:
+
+};
+};
+
+#endif /* SHM_SET_H_ */
