744032c8522af202a90d4fbd16cb297af6ded0d3
CharellKing <CharellkingQu@gmail.com>
2014-08-06 12:39:09 +0800

    add hash function

---
+++ src/shm/shm_hash.h
@@ -0,0 +1,60 @@
+/*
+ * shm_hash.h
+ *
+ *  Created on: 2014年8月6日
+ *      Author: root
+ */
+
+#ifndef _SHM_SHM_HASH_H_
+#define _SHM_SHM_HASH_H_
+
+/* COPYRIGHT: Copyright 2014 CharellkingQu
+ * LICENSE: GPL
+ * AUTHOR: CharellkingQu
+ * DATE : 2014-04-15
+ */
+
+#ifndef _SHM_HASH_H_
+
+#define _SHM_HASH_H_
+
+namespace TimePass {
+namespace Hash {
+off_t HashFunc(const char* s) {
+  off_t h = 0;
+  while (*s) {
+    h = 5 * h + *s;
+    ++s;
+  }
+  return h;
+}
+
+off_t HashFunc(char c) {
+  return c;
+}
+
+off_t HashFunc(unsigned char c) {
+  return c;
+}
+
+off_t HashFunc(int16_t x) {
+  return x;
+}
+
+off_t HashFunc(u_int16_t x) {
+  return x;
+}
+
+off_t HashFunc(int32_t x) {
+  return x;
+}
+
+off_t HashFunc(u_int32_t x) {
+  return x;
+}
+}; /*namespace Hash*/
+}; /*namespace TimePass*/
+
+
+
+#endif /* _SHM_SHM_HASH_H_ */
